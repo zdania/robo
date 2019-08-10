bd374d1c5976d46414f05b5bcd21a24b21b438a6
zdania <and@infosolutions.com.pl>
2019-08-10 06:46:27 +0200

    secrets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,7 +93,7 @@ Rails.application.configure do
     :address              => "smtp.gmail.com",
     :port                 => 587,
     :user_name            => ENV['SMTP_USERNAME'],
-    :password             => ENV['SMTP_PASSWORD']
+    :password             => ENV['SMTP_PASSWORD'],
     :authentication       => "plain",
     :enable_starttls_auto => true
     }
