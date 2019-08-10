98729a94cbfe4cbca7f5f1fbfad3de4ca8f0453c
zdania <and@infosolutions.com.pl>
2019-08-10 06:35:54 +0200

    ignore

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,8 +92,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
     :port                 => 587,
-    :user_name            => 'twojemail@gmail.com',
-    :password             => 'abcdefg',
+    :user_name            => ENV['SMTP_USERNAME'],
+    :password             => ENV['SMTP_PASSWORD']
     :authentication       => "plain",
     :enable_starttls_auto => true
     }
