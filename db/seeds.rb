fbf6bc71aa2e2d93d480c95aa7aa4e33c369d04e
zdania <and@infosolutions.com.pl>
2019-08-18 20:37:00 +0200

    pp

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,7 @@ print "Product "
 200.times do
   product = Product.create! name: "#{Faker::Hacker.verb} #{Faker::Hacker.noun}".capitalize,
     description: Faker::Hacker.say_something_smart,
-    long_description: Faker::Lorem.paragraphs(3).join("\n\n"),
+    long_description: Faker::Lorem.paragraphs.join("\n\n"),
     category_id: category_ids.sample,
     price: Faker::Number.decimal(3, 2)
   product.remote_photo_url = Faker::Avatar.image(
